29a23c950cb417458ecfdbcef3f71170d6dacaed
Administrator <Administrator@scsbwin-76543.ITTPA.EBIZ.VERIZON.COM>
2015-07-29 09:04:57 -0700

    Css and JS added for styles

---
+++ src/main/java/com/vz/tg/services/Impl/HomeServiceImpl.java
@@ -0,0 +1,15 @@
+package com.vz.tg.services.Impl;
+
+import org.apache.solr.client.solrj.SolrResponse;
+
+import com.vz.tg.services.HomeService;
+
+public class HomeServiceImpl implements HomeService {
+
+	@Override
+	public SolrResponse getServiceResponse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
