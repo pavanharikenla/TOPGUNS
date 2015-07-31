11e8802a4eb1bd27ed66a20cc30cdfd81c9ef4de
pavan.harikenla@gmail.com <pavan.harikenla@gmail.com>
2015-07-31 04:23:48 -0700

    end point information updated

--- src/main/java/com/vz/tg/connectors/SolrConnector.java
+++ src/main/java/com/vz/tg/connectors/SolrConnector.java
@@ -6,7 +6,7 @@ import org.apache.solr.client.solrj.impl.HttpSolrClient;
 
 public class SolrConnector {
 	public static SolrClient getConnection(){
-		String url = "http://localhost:8983/solr/tg_core";
+		String url = "http://113.128.164.246:8983/solr/tg_core";
 		SolrClient solr = new HttpSolrClient(url);
 		return solr;
 	}
