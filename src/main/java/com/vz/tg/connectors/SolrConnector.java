61f26765cec30c71a730bd98ba2b8dc10b5116db
pavanharikenla <pavan.harikenla@gmail.com>
2015-08-01 13:39:41 +0530

    Update SolrConnector.java
    solr end point updated

--- src/main/java/com/vz/tg/connectors/SolrConnector.java
+++ src/main/java/com/vz/tg/connectors/SolrConnector.java
@@ -6,7 +6,7 @@ import org.apache.solr.client.solrj.impl.HttpSolrClient;
 
 public class SolrConnector {
 	public static SolrClient getConnection(){
-		String url = "http://113.128.164.246:8983/solr/tg_core";
+		String url = "http://ec2-52-10-143-10.us-west-2.compute.amazonaws.com:8080/solr/core0";
 		SolrClient solr = new HttpSolrClient(url);
 		return solr;
 	}
